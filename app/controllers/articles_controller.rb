1b2e70883e383c87a90e66f20fda4d89dbe22087
Rojina Thapa <rojinat@ekbana.com>
2017-05-29 11:09:24 +0545

    commets featured enabled

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,33 +8,36 @@ class ArticlesController < ApplicationController
 		@article =Article.new
 	end
 	def create
-  @article = Article.new(article_params)
-  @article.save
-  flash.notice = "Article '#{@article.title}' Added!"
-  redirect_to article_path(@article)
-end
-def edit
-  @article = Article.find(params[:id])
-end
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Article '#{@article.title}' Added!"
+		redirect_to article_path(@article)
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
 
 
-def show
-	@article = Article.find(params[:id])
+	def show
+		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id
 
-end
-def update
-@article = Article.find(params[:id])
-@article.update(article_params)
-flash.notice = "Article '#{@article.title}' Updated!"
-redirect_to article_path(@article)
 
-end
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "Article '#{@article.title}' Updated!"
+		redirect_to article_path(@article)
 
-def destroy
-@article=Article.find(params[:id])
-@article.destroy
-flash.notice= " Article successfully deleted!"
-redirect_to action: "index"
+	end
+
+	def destroy
+		@article=Article.find(params[:id])
+		@article.destroy
+		flash.notice= " Article successfully deleted!"
+		redirect_to action: "index"
 	end
 
 end
